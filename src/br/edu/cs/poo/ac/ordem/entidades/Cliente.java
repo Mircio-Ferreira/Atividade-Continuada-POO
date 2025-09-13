d5f8a009c4910629ecf3f09abec3e0f3766a92b7
Mircio <mfsn@cesar.school>
2025-09-13 17:04:20 -0300

    first commit

---
+++ src/br/edu/cs/poo/ac/ordem/entidades/Cliente.java
@@ -0,0 +1,48 @@
+package br.edu.cs.poo.ac.ordem.entidades;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Cliente {
+
+	private String cpfCnpj;
+	private String nome;
+	private Contato contato;
+	private LocalDate dataCadastro;
+	
+	public Cliente(String cpfCnpj,String nome, Contato contato,LocalDate dataCadastro) {
+		this.cpfCnpj=cpfCnpj;
+		this.nome=nome;
+		this.contato=contato;
+		this.dataCadastro=dataCadastro;
+	}
+	
+	public void setNome(String nome) {
+		this.nome=nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setContato(Contato contato) {
+		this.contato=contato;
+	}
+	
+	public Contato getContato() {
+		return contato;
+	}
+	
+	public String getCpfCnpj() {
+		return cpfCnpj;
+	}
+	
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
+	
+	public int getIdadeCadastro() {
+		return Period.between(dataCadastro, LocalDate.now()).getYears();
+	}
+	
+}
