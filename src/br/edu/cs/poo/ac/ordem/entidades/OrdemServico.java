80c9237d5ebccb049ac55b46c1c38597be9490f2
Mircio <mfsn@cesar.school>
2025-09-21 17:00:16 -0300

    alterando retorno de ordem de servico

--- src/br/edu/cs/poo/ac/ordem/entidades/OrdemServico.java
+++ src/br/edu/cs/poo/ac/ordem/entidades/OrdemServico.java
@@ -34,18 +34,18 @@ public class OrdemServico implements Serializable {
 
 		String concatenacao = "";
 		if (notebook != null) {
-			concatenacao = notebook.getIdTipo();
-		} else if (desktop != null) {
-			concatenacao = desktop.getIdTipo();
+			concatenacao += notebook.getIdTipo();
+		} else{
+			concatenacao += desktop.getIdTipo();
 		}
 
 	    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
 	    concatenacao += dataHoraAbertura.format(formatter);
 
-		if (cliente.getCpfCnpj().length() == 11) {
-			concatenacao += "000" + cliente.getCpfCnpj();
-		} else {
+		if (cliente.getCpfCnpj().length() > 11) {
 			concatenacao += cliente.getCpfCnpj();
+		} else {
+			concatenacao += "000" + cliente.getCpfCnpj();
 		}
 
 		return concatenacao;
