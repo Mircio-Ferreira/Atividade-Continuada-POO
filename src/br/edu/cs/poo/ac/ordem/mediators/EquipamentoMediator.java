0988bde566053071b59e40877f252c72d708672a
Mircio <mfsn@cesar.school>
2025-10-11 15:36:11 -0300

    alterando equipamentoMediator

--- src/br/edu/cs/poo/ac/ordem/mediators/EquipamentoMediator.java
+++ src/br/edu/cs/poo/ac/ordem/mediators/EquipamentoMediator.java
@@ -1,36 +1,52 @@
 package br.edu.cs.poo.ac.ordem.mediators;
 
+import br.edu.cs.poo.ac.ordem.entidades.Desktop;
+import br.edu.cs.poo.ac.ordem.entidades.Notebook;
+import br.edu.cs.poo.ac.ordem.daos.NotebookDAO;
+import br.edu.cs.poo.ac.ordem.daos.DesktopDAO;
+
 public class EquipamentoMediator {
-	public ResultadoMediator incluirDesktop(Desktop desk) {
-	}
+    
+    private static EquipamentoMediator instancia;
+    
+    private NotebookDAO notebookDao;
+    private DesktopDAO desktopDAO;
+    
+    public EquipamentoMediator() {
+        notebookDao=new NotebookDAO();
+        desktopDAO = new DesktopDAO();
+    }
+    
+    public ResultadoMediator incluirDesktop(Desktop desk) {
+    }
 
-	public ResultadoMediator alterarDesktop(Desktop desk) {
-	}
+    public ResultadoMediator alterarDesktop(Desktop desk) {
+    }
 
-	public ResultadoMediator incluirNotebook(Notebook note) {
-	}
+    public ResultadoMediator incluirNotebook(Notebook note) {
+    }
 
-	public ResultadoMediator alterarNotebook(Notebook note) {
-	}
+    public ResultadoMediator alterarNotebook(Notebook note) {
+    }
 
-	public ResultadoMediator excluirNotebook(String idTipoSerial) {
-	}
+    public ResultadoMediator excluirNotebook(String idTipoSerial) {
+    }
 
-	public ResultadoMediator excluirDesktop(String idTipoSerial) {
-	}
+    public ResultadoMediator excluirDesktop(String idTipoSerial) {
+    }
 
-	public Notebook buscarNotebook(String idTipoSerial) {
-	}
+    public Notebook buscarNotebook(String idTipoSerial) {
+    }
 
-	public Desktop buscarDesktop(String idTipoSerial) {
-	}
+    public Desktop buscarDesktop(String idTipoSerial) {
+    }
 
-	public ResultadoMediator validarDesktop(Desktot desk) {
-	}
+    public ResultadoMediator validarDesktop(Desktot desk) {
+    }
 
-	public ResultadoMediator validarDesktop(Desktot desk) {
-	}
+    public ResultadoMediator validarDesktop(Desktot desk) {
+    }
 
-	public ResultadoMediator validar(DadosEquipamento equip) {
-	}
+    public ResultadoMediator validar(DadosEquipamento equip) {
+    }
 }
